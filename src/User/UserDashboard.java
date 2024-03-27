5649cb14f9e1f8a7ce2da479e58b446ad311d8a5
errizky <errizkyaulia@gmail.com>
2024-03-27 12:54:55 +0700

    Initial commit

---
+++ src/User/UserDashboard.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package User;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Rizky
+ */
+public class UserDashboard {
+
+    public void showWelcomeMessage() {
+        // Menampilkan pesan "Welcome" menggunakan JOptionPane
+        JOptionPane.showMessageDialog(null, "Welcome to the User Dashboard!");
+    }
+    
+}
---
+++ src/app_funding_v1/App_funding_v1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app_funding_v1;
+
+/**
+ *
+ * @author Rizky
+ */
+public class App_funding_v1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Membuat objek untuk frame login
+        Login loginFrame = new Login();
+        
+        // Menampilkan frame login
+        loginFrame.setVisible(true);
+    }
+    
+}
